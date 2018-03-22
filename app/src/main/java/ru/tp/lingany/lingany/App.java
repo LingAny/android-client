f896f6785b5363e15af6981a0903115cbceac967
ZubAnt <ya.zubarevanton@yandex.ru>
2018-03-22 20:26:45 +0300

    create ReflectionActivity

--- app/src/main/java/ru/tp/lingany/lingany/App.java
+++ app/src/main/java/ru/tp/lingany/lingany/App.java
@@ -15,7 +15,7 @@ public class App extends Application {
         super.onCreate();
         Log.i("App", "onCreate");
 
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        Intent intent = new Intent(getApplicationContext(), ReflectionActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
---
+++ app/src/main/java/ru/tp/lingany/lingany/ReflectionActivity.java
@@ -0,0 +1,17 @@
+package ru.tp.lingany.lingany;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by anton on 22.03.18.
+ */
+
+public class ReflectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reflection);
+    }
+}
