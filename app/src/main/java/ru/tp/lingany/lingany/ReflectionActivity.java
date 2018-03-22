25df854b488a4ca2e2d7f2959a6a46a72173d01b
ZubAnt <ya.zubarevanton@yandex.ru>
2018-03-22 21:09:20 +0300

    bool shit

--- app/src/main/java/ru/tp/lingany/lingany/ReflectionActivity.java
+++ app/src/main/java/ru/tp/lingany/lingany/ReflectionActivity.java
@@ -2,6 +2,11 @@ package ru.tp.lingany.lingany;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by anton on 22.03.18.
@@ -13,5 +18,26 @@ public class ReflectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reflection);
+
+        List<String> items = new ArrayList<>();
+        fillItems(items);
+
+        RecyclerView recyclerView = findViewById(R.id.native_lang_rv_id);
+        ReflectionAdapter adapter = new ReflectionAdapter(items);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+
+    }
+
+    private void fillItems(List<String> items) {
+        items.add("Russian");
+        items.add("English");
+        items.add("German");
+        items.add("Spanish");
+        items.add("French");
+        items.add("Italian");
     }
 }
