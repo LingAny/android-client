164815185a4c13390960924213d8797c27f9d222
pitikdmitry <pitik.dmitry@mail.ru>
2018-04-16 10:28:14 +0300

    add list of random words to category sdk model

--- app/src/main/java/ru/tp/lingany/lingany/sdk/categories/Category.java
+++ app/src/main/java/ru/tp/lingany/lingany/sdk/categories/Category.java
@@ -1,6 +1,7 @@
 package ru.tp.lingany.lingany.sdk.categories;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.UUID;
 
 import ru.tp.lingany.lingany.sdk.reflections.Reflection;
@@ -16,11 +17,14 @@ public class Category implements Serializable {
 
     private Reflection reflection;
 
+    private List<String> randomWords;
+
     public Category(UUID id, String href, String title, Reflection reflection) {
         this.id = id;
         this.href = href;
         this.title = title;
         this.reflection = reflection;
+        fillRandomWords();
     }
 
     public UUID getId() {
@@ -37,5 +41,18 @@ public class Category implements Serializable {
 
     public Reflection getReflection() {
         return reflection;
+    }
+
+    public void getRandomWords() {
+        if (randomWords.size() < 1) {
+            fillrandomWords();
+        }
+        String word = randomWords.get(0);
+        randomWords.remove(0);
+        return word;
+    }
+
+    private void fillrandomWords() {
+
     }
 }
