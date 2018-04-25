1f4c0aba31333dffd10676b37363537ebc40990c
ZubAnt <ya.zubarevanton@yandex.ru>
2018-04-25 23:59:25 +0300

    create empty ChooseReflectionActivity

---
+++ app/src/main/java/ru/tp/lingany/lingany/activities/ChooseReflectionActivity.java
@@ -0,0 +1,14 @@
+package ru.tp.lingany.lingany.activities;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ChooseReflectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
