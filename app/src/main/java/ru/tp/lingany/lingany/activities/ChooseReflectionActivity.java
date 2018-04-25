16b5c8e970f451ec93025d158fe66441b3df2172
ZubAnt <ya.zubarevanton@yandex.ru>
2018-04-26 00:31:14 +0300

    create newInstance in ChooseNativeLanguageFragment

--- app/src/main/java/ru/tp/lingany/lingany/activities/ChooseReflectionActivity.java
+++ app/src/main/java/ru/tp/lingany/lingany/activities/ChooseReflectionActivity.java
@@ -2,13 +2,20 @@ package ru.tp.lingany.lingany.activities;
 
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+
+import ru.tp.lingany.lingany.R;
 
 public class ChooseReflectionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_choose_reflection);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
 
     }
 }
--- app/src/main/java/ru/tp/lingany/lingany/fragments/ChooseLanguageFragment.java
+++
@@ -1,7 +0,0 @@
-package ru.tp.lingany.lingany.fragments;
-
-import android.support.v4.app.Fragment;
-
-public class ChooseLanguageFragment  extends Fragment {
-
-}
