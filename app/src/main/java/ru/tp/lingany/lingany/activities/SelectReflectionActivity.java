7412e0d33ef88d9b6814fd1911eec318b147a5c4
zubant <ya.zubarevnton@yandex.ru>
2018-04-26 16:11:44 +0300

    create onForeignLangClick

--- app/src/main/java/ru/tp/lingany/lingany/activities/SelectReflectionActivity.java
+++ app/src/main/java/ru/tp/lingany/lingany/activities/SelectReflectionActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ru.tp.lingany.lingany.R;
@@ -13,11 +14,15 @@ import ru.tp.lingany.lingany.fragments.SelectNativeLangFragment;
 import ru.tp.lingany.lingany.fragments.SelectForeignLangFragment;
 import ru.tp.lingany.lingany.sdk.languages.Language;
 
-public class SelectReflectionActivity extends AppCompatActivity
-        implements SelectNativeLangFragment.NativeLangClickListener {
+public class SelectReflectionActivity extends AppCompatActivity implements
+        SelectNativeLangFragment.NativeLangClickListener,
+        SelectForeignLangFragment.ForeignLangClickListener {
+
+    private Language nativeLang;
+
+    private Language foreignLang;
 
     private List<Language> supportedLanguages;
-    private Integer nativeLangIdx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +37,23 @@ public class SelectReflectionActivity extends AppCompatActivity
 
     @Override
     public void onNativeLangClick(View view, List<Language> supportedLanguages, int position) {
+        Toast.makeText(SelectReflectionActivity.this, "onNativeLangClick", Toast.LENGTH_SHORT).show();
+
         this.supportedLanguages = supportedLanguages;
-        this.nativeLangIdx = position;
-        Toast.makeText(SelectReflectionActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+
+        List<Language> foreignLanguages = new ArrayList<>(supportedLanguages);
+        nativeLang = supportedLanguages.get(position);
+        foreignLanguages.remove(position);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, SelectForeignLangFragment.getInstance(foreignLanguages))
+                .commit();
+    }
+
+    @Override
+    public void onForeignLangClick(View view, int position) {
+        Toast.makeText(SelectReflectionActivity.this, "onForeignLangClick", Toast.LENGTH_SHORT).show();
+        foreignLang = supportedLanguages.get(position);
     }
 }
