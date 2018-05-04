00da0c186984106babb802ca91407babb07f9a68
zubant <ya.zubarevnton@yandex.ru>
2018-05-04 15:17:23 +0300

    try inflate category

--- app/src/main/java/ru/tp/lingany/lingany/activities/CategoryActivity.java
+++ app/src/main/java/ru/tp/lingany/lingany/activities/CategoryActivity.java
@@ -3,13 +3,23 @@ package ru.tp.lingany.lingany.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.SparseIntArray;
+import android.view.MenuItem;
 
 import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.ParsedRequestListener;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
@@ -35,6 +45,22 @@ public class CategoryActivity extends AppCompatActivity implements
 
     public static final String CATEGORIES = "CATEGORIES";
 
+    private BottomNavigationViewEx bnve;
+    private ViewPager vp;
+    private VpAdapter vpAdapter;
+
+    private static final String BRAIN_STORM_PAGE_POSITION = "BRAIN_STORM_PAGE_POSITION";
+    private static final String SHAPE_PAGE_POSITION = "SHAPE_PAGE_POSITION";
+    private static final String REPEAT_PAGE_POSITION = "REPEAT_PAGE_POSITION";
+
+    private final HashMap<String, Integer> itemPositionMap;
+    {
+        itemPositionMap = new HashMap<>();
+        itemPositionMap.put(BRAIN_STORM_PAGE_POSITION, 0);
+        itemPositionMap.put(SHAPE_PAGE_POSITION, 1);
+        itemPositionMap.put(REPEAT_PAGE_POSITION, 2);
+    }
+
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         if (categories != null) {
@@ -51,17 +77,17 @@ public class CategoryActivity extends AppCompatActivity implements
 
         refId = getRefId();
         loadingFragment = new LoadingFragment();
-        initBottomNavigationBar();
+        initBottomNavigationBarWithViewPager();
 
-        if (savedInstanceState != null) {
-            categories = (List<Category>) savedInstanceState.getSerializable(CATEGORIES);
-            if (categories != null) {
-                inflateSelectCategoryFragment();
-                return;
-            }
-        }
-
-        inflateLoadingFragment();
+//        if (savedInstanceState != null) {
+//            categories = (List<Category>) savedInstanceState.getSerializable(CATEGORIES);
+//            if (categories != null) {
+//                inflateSelectCategoryFragment();
+//                return;
+//            }
+//        }
+//
+//        inflateLoadingFragment();
         getCategoriesForReflection();
     }
 
@@ -118,10 +144,11 @@ public class CategoryActivity extends AppCompatActivity implements
     }
 
     private void inflateSelectCategoryFragment() {
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.container, SelectCategoryFragment.getInstance(categories))
-                .commit();
+//        getSupportFragmentManager()
+//                .beginTransaction()
+//                .replace(R.id.container, )
+//                .commit();
+        vpAdapter.replaceItem(SelectCategoryFragment.getInstance(categories), itemPositionMap.get(BRAIN_STORM_PAGE_POSITION));
     }
 
     private void inflateLoadingFragment() {
@@ -140,12 +167,86 @@ public class CategoryActivity extends AppCompatActivity implements
             }
         }, delayMillis);
     }
-    
-    private void initBottomNavigationBar() {
-        BottomNavigationViewEx bnve = findViewById(R.id.bottom_navigation);
+
+    @SuppressWarnings("unchecked")
+    private void initBottomNavigationBarWithViewPager() {
+        vp = findViewById(R.id.vp);
+        bnve = findViewById(R.id.bottom_navigation);
+
+        int iconSize = getResources().getInteger(R.integer.bottomNavBarIconSize);
+        int itemHeight = getResources().getInteger(R.integer.bottomNavBarItemHeight);
+
         bnve.setTextVisibility(false);
-        int iconSize = 32;
         bnve.setIconSize(iconSize, iconSize);
-        bnve.setItemHeight(BottomNavigationViewEx.dp2px(this, iconSize + 16));
+        bnve.setItemHeight(BottomNavigationViewEx.dp2px(this, itemHeight));
+
+        int size = 3;
+        ArrayList fragments = new ArrayList<>(size);
+        final SparseIntArray items = new SparseIntArray(size);
+
+        fragments.add(new LoadingFragment());
+        fragments.add(new LoadingFragment());
+        fragments.add(new LoadingFragment());
+
+        items.put(R.id.action_categories, 0);
+        items.put(R.id.action_shape, 1);
+        items.put(R.id.action_hot, 2);
+
+        vpAdapter = new VpAdapter(getSupportFragmentManager(), fragments);
+        vp.setAdapter(vpAdapter);
+
+        // set listener to change the current item of view pager when click bottom nav item
+        bnve.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            private int previousPosition = -1;
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int position = items.get(item.getItemId());
+                if (previousPosition != position) {
+                    previousPosition = position;
+                    vp.setCurrentItem(position);
+                }
+                return true;
+            }
+        });
+
+        // set listener to change the current checked item of bottom nav when scroll view pager
+        vp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                bnve.setCurrentItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+    }
+
+    private static class VpAdapter extends FragmentPagerAdapter {
+        private List<Fragment> data;
+
+        public VpAdapter(FragmentManager fm, List<Fragment> data) {
+            super(fm);
+            this.data = data;
+        }
+
+        public void replaceItem(Fragment fragment, int position) {
+            data.set(position, fragment);
+        }
+
+        @Override
+        public int getCount() {
+            return data.size();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return data.get(position);
+        }
     }
 }
