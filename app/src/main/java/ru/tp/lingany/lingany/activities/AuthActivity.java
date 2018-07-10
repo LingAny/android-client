543de8666b0c61be01150e5b87f24b9abedfd5f7
pitikdmitry <pitik.dmitry@mail.ru>
2018-07-10 16:57:01 +0300

    recieved response

--- app/src/main/java/ru/tp/lingany/lingany/activities/AuthActivity.java
+++ app/src/main/java/ru/tp/lingany/lingany/activities/AuthActivity.java
@@ -1,8 +1,17 @@
 package ru.tp.lingany.lingany.activities;
 
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import net.openid.appauth.AuthorizationRequest;
+import net.openid.appauth.AuthorizationService;
+import net.openid.appauth.AuthorizationServiceConfiguration;
 
 import ru.tp.lingany.lingany.R;
 import ru.tp.lingany.lingany.fragments.AuthFragment;
@@ -11,7 +20,7 @@ import ru.tp.lingany.lingany.fragments.AuthFragment;
 public class AuthActivity extends AppCompatActivity {
 
     private AuthFragment authFragment;
-
+    public static final String LOG_TAG = "AppAuthSample";
 //    @Override
 //    public void onSaveInstanceState(Bundle savedInstanceState) {
 //        if (supportedLanguages != null) {
@@ -39,10 +48,28 @@ public class AuthActivity extends AppCompatActivity {
         inflateAuthFragment();
     }
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        authFragment.checkIntent(getIntent());
+    }
     private void inflateAuthFragment() {
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.container, authFragment)
                 .commit();
     }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        System.out.println("NEW INTENT");
+        System.out.println("NEW INTENT");
+        System.out.println("NEW INTENT");
+        System.out.println("NEW INTENT");
+        System.out.println("NEW INTENT");
+        authFragment.checkIntent(intent);
+    }
+
+
 }
--- app/src/main/java/ru/tp/lingany/lingany/fragments/AuthFragment.java
+++ app/src/main/java/ru/tp/lingany/lingany/fragments/AuthFragment.java
@@ -4,10 +4,14 @@ package ru.tp.lingany.lingany.fragments;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.RestrictionsManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.UserManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
 import android.util.Log;
@@ -15,11 +19,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import net.openid.appauth.AuthState;
+import net.openid.appauth.AuthorizationException;
 import net.openid.appauth.AuthorizationRequest;
+import net.openid.appauth.AuthorizationResponse;
 import net.openid.appauth.AuthorizationService;
 import net.openid.appauth.AuthorizationServiceConfiguration;
+import net.openid.appauth.TokenResponse;
 import net.orange_box.storebox.StoreBox;
 
 import org.json.JSONException;
@@ -37,6 +45,8 @@ public class AuthFragment extends Fragment {
     private LayoutInflater inflater;
     // state
     AuthState mAuthState;
+    private static final String USED_INTENT = "USED_INTENT";
+    public static final String LOG_TAG = "AppAuthSample";
 
     @Nullable
     @Override
@@ -45,6 +55,7 @@ public class AuthFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_auth, container, false);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
@@ -66,7 +77,6 @@ public class AuthFragment extends Fragment {
                 Uri.parse("https://accounts.google.com/o/oauth2/v2/auth") /* auth endpoint */,
                 Uri.parse("https://www.googleapis.com/oauth2/v4/token") /* token endpoint */
         );
-        AuthorizationService authorizationService = new AuthorizationService(view.getContext());
         String clientId = "511828570984-fuprh0cm7665emlne3rnf9pk34kkn86s.apps.googleusercontent.com";
         Uri redirectUri = Uri.parse("com.google.codelabs.appauth:/oauth2callback");
         AuthorizationRequest.Builder builder = new AuthorizationRequest.Builder(
@@ -77,15 +87,14 @@ public class AuthFragment extends Fragment {
         );
         builder.setScopes("profile");
 
-//        if(mMainActivity.getLoginHint() != null){
-//            Map loginHintMap = new HashMap<String, String>();
-//            loginHintMap.put(LOGIN_HINT,mMainActivity.getLoginHint());
-//            builder.setAdditionalParameters(loginHintMap);
-//
-//            Log.i(LOG_TAG, String.format("login_hint: %s", mMainActivity.getLoginHint()));
-//        }
-
         AuthorizationRequest request = builder.build();
+
+        AuthorizationService authorizationService = new AuthorizationService(view.getContext());
+
+        Log.i(LOG_TAG, "DOING REQUEST");
+        Log.i(LOG_TAG, "DOING REQUEST");
+        Log.i(LOG_TAG, "DOING REQUEST");
+        Log.i(LOG_TAG, "DOING REQUEST");
         String action = "com.google.codelabs.appauth.HANDLE_AUTHORIZATION_RESPONSE";
         Intent postAuthorizationIntent = new Intent(action);
         PendingIntent pendingIntent = PendingIntent.getActivity(view.getContext(), request.hashCode(), postAuthorizationIntent, 0);
@@ -124,6 +133,54 @@ public class AuthFragment extends Fragment {
         return null;
     }
 
-    private void getAppRestrictions() {
+    public void checkIntent(@Nullable Intent intent) {
+        if (intent != null) {
+            String action = intent.getAction();
+            if (action == null) {
+                return;
+            }
+            googleButton.setVisibility(View.GONE);
+            switch (action) {
+                case "com.google.codelabs.appauth.HANDLE_AUTHORIZATION_RESPONSE":
+                    if (!intent.hasExtra(USED_INTENT)) {
+                        handleAuthorizationResponse(intent);
+                        intent.putExtra(USED_INTENT, true);
+                    }
+                    break;
+                default:
+                    // do nothing
+            }
+        }
+    }
+
+    private void handleAuthorizationResponse(@NonNull Intent intent) {
+        AuthorizationResponse response = AuthorizationResponse.fromIntent(intent);
+        AuthorizationException error = AuthorizationException.fromIntent(intent);
+        final AuthState authState = new AuthState(response, error);
+        if (response != null) {
+            Log.i(LOG_TAG, String.format("Handled Authorization Response %s ", authState.toJsonString()));
+            AuthorizationService service = new AuthorizationService(this.getActivity());
+            service.performTokenRequest(response.createTokenExchangeRequest(), new AuthorizationService.TokenResponseCallback() {
+                @Override
+                public void onTokenRequestCompleted(@Nullable TokenResponse tokenResponse, @Nullable AuthorizationException exception) {
+                    if (exception != null) {
+                        Log.w(LOG_TAG, "Token Exchange failed", exception);
+                    } else {
+                        if (tokenResponse != null) {
+                            authState.update(tokenResponse, exception);
+                            persistAuthState(authState);
+                            Log.i(LOG_TAG, String.format("Token Response [ Access Token: %s, ID Token: %s ]", tokenResponse.accessToken, tokenResponse.idToken));
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    private void persistAuthState(@NonNull AuthState authState) {
+//        getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE).edit()
+//                .putString(AUTH_STATE, authState.toJsonString())
+//                .commit();
+        enablePostAuthorizationFlows();
     }
 }
